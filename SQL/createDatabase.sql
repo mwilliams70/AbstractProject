3682b55341a09e6bd67087e95a0107b9214369bb
Michael Williams <templehillride@gmail.com>
2025-04-07 15:30:50 -0400

    tables created

--- SQL/createDatabase.sql
+++ SQL/createDatabase.sql
@@ -22,7 +22,7 @@ CREATE TABLE Faculty (
 );
 CREATE TABLE Interest (
     interestID INT AUTO_INCREMENT PRIMARY KEY,
-    content TEXT
+    content VARCHAR(25)
 );
 CREATE TABLE College (
     collegeID INT PRIMARY KEY,
@@ -39,12 +39,74 @@ CREATE TABLE FacultyInterest (
 );
 
 
-
 CREATE TABLE CollegeFaculty (
     collegeID INT,
     facultyID INT,
     PRIMARY KEY (collegeID, facultyID),
     FOREIGN KEY (collegeID) REFERENCES College(collegeID),
     FOREIGN KEY (facultyID) REFERENCES Faculty(facultyID)
+);
+
+CREATE TABLE Abstract (
+    abstractID INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(50),
+    author VARCHAR(255),
+    content TEXT
+);
+
+CREATE TABLE FacultyAbstract (
+    facultyID INT,
+    abstractID INT,
+    PRIMARY KEY (facultyID, abstractID),
+    FOREIGN KEY (facultyID) REFERENCES Faculty(facultyID),
+    FOREIGN KEY (abstractID) REFERENCES Abstract(abstractID)
+);
+
+CREATE TABLE PublicUser (
+    publicUserID INT AUTO_INCREMENT PRIMARY KEY, 
+    organizationName VARCHAR(50),
+    contactInfo VARCHAR(100)
+);
+
+CREATE TABLE PublicUserInterest (
+    publicUserID INT,
+    interestID INT,
+    PRIMARY KEY (publicUserID, interestID),
+    FOREIGN KEY (publicUserID) REFERENCES PublicUser(publicUserID),
+    FOREIGN KEY (interestID) REFERENCES Interest(interestID)
+);
+
+CREATE TABLE Student (
+    studentID INT AUTO_INCREMENT PRIMARY KEY, 
+    firstName VARCHAR(50),
+    lastName VARCHAR(50),
+    email VARCHAR(50)
+);
+
+CREATE TABLE StudentMajor (
+    collegeID INT,
+    studentID INT,
+    major VARCHAR(50),
+    PRIMARY KEY (collegeID, studentID),
+    FOREIGN KEY (studentID) REFERENCES Student(studentID),
+    FOREIGN KEY (collegeID) REFERENCES College(collegeID)
+);
+
+CREATE TABLE StudentInterest (
+    studentID INT,
+    interestID INT,
+    PRIMARY KEY (studentID, interestID),
+    FOREIGN KEY (studentID) REFERENCES Student(studentID),
+    FOREIGN KEY (interestID) REFERENCES Interest(interestID)
+);
+
+CREATE TABLE Account (
+    username VARCHAR(50) PRIMARY KEY,
+    password VARCHAR(255),
+    facultyID INT,
+    studentID INT,
+    publicUserID INT,
+    FOREIGN KEY (facultyID) REFERENCES Faculty(facultyID),
+    FOREIGN KEY (publicUserID) REFERENCES PublicUser(publicUserID)
 );
 
