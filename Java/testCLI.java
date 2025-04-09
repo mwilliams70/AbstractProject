b4851e3bb19df0d9d1934f20f272dabeee10ea67
Michael Williams <templehillride@gmail.com>
2025-04-09 12:23:31 -0400

    faculty account generation created

--- Java/testCLI.java
+++ Java/testCLI.java
@@ -39,39 +39,62 @@ public class testCLI {
     }
 
     public static String getMostRecentID(String role) {
+        // use "student", "faculty", or "publicUser" for role
         return "SELECT MAX(" + role + "ID) FROM " + role;
     }
 
-    public void createAccount(String un, String pswd, String firstName, String lastName, String email, int role) {
-        switch (role) {
-            case 1:
-                break;
-                
-            case 2:
-                String insertStudent = "INSERT INTO student VALUES (NULL, ?, ?, ?)";
-                String getID = getMostRecentID("student");
-                try (PreparedStatement stmt = conn.prepareStatement(insertStudent)) {
-                    stmt.setString(1, firstName);
-                    stmt.setString(2, lastName);
-                    stmt.setString(3, email);
-                    stmt.executeUpdate();
-                    PreparedStatement id = conn.prepareStatement(getID);
-                    ResultSet sID = id.executeQuery();
-                    if (sID.next()) {
-                        String insertAccount = "INSERT INTO account (username, password, studentID) VALUES(?, ?, ?)";
-                        PreparedStatement ia = conn.prepareStatement(insertAccount);
-                        ia.setString(1, un);
-                        ia.setString(2, pswd);
-                        ia.setInt(3, sID.getInt(1));
-                        ia.executeUpdate();
-                    }                
-                } catch (SQLException e) {
-                    e.printStackTrace();                   
-                }               
-            case 3:
-                break;
+    public void insertAccount(String table, String un, String pswd, int id) {
+        String insertAccount = "INSERT INTO account (username, password, " + table + "ID) VALUES(?, ?, ?)";
+        try {
+            PreparedStatement ia = conn.prepareStatement(insertAccount);
+            ia.setString(1, un);
+            ia.setString(2, pswd);
+            ia.setInt(3, id);
+            ia.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
-    } 
+        
+    }
+
+    public void createFacultyAccount(String un, String pswd, String firstName, String lastName, String email, String department, int officeNum) {
+        String insertFaculty = "INSERT INTO faculty VALUES (NULL, ?, ?, ?, ?, ?)";
+        String getID = getMostRecentID("faculty");
+        try (PreparedStatement stmt = conn.prepareStatement(insertFaculty)) {
+            stmt.setString(1, firstName);
+            stmt.setString(2, lastName);
+            stmt.setString(3, email);
+            stmt.setString(4, department);
+            stmt.setInt(5, officeNum);
+            stmt.executeUpdate();
+            PreparedStatement id = conn.prepareStatement(getID);
+            ResultSet fID = id.executeQuery();
+            if (fID.next()) {
+                insertAccount("faculty", un, pswd, fID.getInt(1));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void createStudentAccount(String un, String pswd, String firstName, String lastName, String email) {
+        String insertStudent = "INSERT INTO student VALUES (NULL, ?, ?, ?)";
+        String getID = getMostRecentID("student");
+        try (PreparedStatement stmt = conn.prepareStatement(insertStudent)) {
+            stmt.setString(1, firstName);
+            stmt.setString(2, lastName);
+            stmt.setString(3, email);
+            stmt.executeUpdate();
+            PreparedStatement id = conn.prepareStatement(getID);
+            ResultSet sID = id.executeQuery();
+            if (sID.next()) {
+                insertAccount("student", un, pswd, sID.getInt(1));
+            }                
+        } catch (SQLException e) {
+            e.printStackTrace();                   
+        }               
+    }
+    
 
     public void login(String un, String pswd) {
 
@@ -81,6 +104,7 @@ public class testCLI {
         testCLI cli = new testCLI();
         cli.connect("abstract_project", "root", "student");
 
-        cli.createAccount("un", "pw", "Michael", "Williams", "msw7476@g.rit.edu", 2);
+        // cli.createStudentAccount("un", "pw", "Michael", "Williams", "msw7476@g.rit.edu");
+        cli.createFacultyAccount("jimhabermas", "password", "Jim", "Habermas", "jhabermas@g.rit.edu", "GCCIS", 345);
     }
 }
