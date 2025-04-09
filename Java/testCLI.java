ed0ae13f92985cccafb12a43525ab9dbeeba0649
Michael Williams <templehillride@gmail.com>
2025-04-09 12:03:09 -0400

    account creation for student

--- Java/testCLI.java
+++ Java/testCLI.java
@@ -1,5 +1,7 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class testCLI {
@@ -36,8 +38,49 @@ public class testCLI {
         System.out.println("I am a faculty member");
     }
 
+    public static String getMostRecentID(String role) {
+        return "SELECT MAX(" + role + "ID) FROM " + role;
+    }
+
+    public void createAccount(String un, String pswd, String firstName, String lastName, String email, int role) {
+        switch (role) {
+            case 1:
+                break;
+                
+            case 2:
+                String insertStudent = "INSERT INTO student VALUES (NULL, ?, ?, ?)";
+                String getID = getMostRecentID("student");
+                try (PreparedStatement stmt = conn.prepareStatement(insertStudent)) {
+                    stmt.setString(1, firstName);
+                    stmt.setString(2, lastName);
+                    stmt.setString(3, email);
+                    stmt.executeUpdate();
+                    PreparedStatement id = conn.prepareStatement(getID);
+                    ResultSet sID = id.executeQuery();
+                    if (sID.next()) {
+                        String insertAccount = "INSERT INTO account (username, password, studentID) VALUES(?, ?, ?)";
+                        PreparedStatement ia = conn.prepareStatement(insertAccount);
+                        ia.setString(1, un);
+                        ia.setString(2, pswd);
+                        ia.setInt(3, sID.getInt(1));
+                        ia.executeUpdate();
+                    }                
+                } catch (SQLException e) {
+                    e.printStackTrace();                   
+                }               
+            case 3:
+                break;
+        }
+    } 
+
+    public void login(String un, String pswd) {
+
+    }
+
     public static void main(String[] args) {
         testCLI cli = new testCLI();
         cli.connect("abstract_project", "root", "student");
+
+        cli.createAccount("un", "pw", "Michael", "Williams", "msw7476@g.rit.edu", 2);
     }
 }
